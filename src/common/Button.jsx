05d150603ea904b62aad24d805c79d5328cd3e1c
Yboda <ybr2265@naver.com>
2022-10-29 03:08:24 +0900

    Add Button

---
+++ src/common/Button.jsx
@@ -0,0 +1,55 @@
+import styled, { css } from "styled-components";
+
+const Button = ({ children, ...props }) => {
+	return <StBtn {...props}>{children}</StBtn>;
+};
+export default Button;
+
+const StBtn = styled.button`
+	cursor: pointer;
+	box-sizing: border-box;
+	border-radius: 4px;
+	display: block;
+	font-size: 14px;
+	font-weight: 700;
+	padding: 5px 9px !important;
+	text-align: center;
+	text-overflow: ellipsis;
+	text-transform: inherit;
+	-webkit-user-select: none;
+
+	${({ variant }) => {
+		switch (variant) {
+			case "smallBlue":
+				return css`
+					width: auto;
+					background-color: #0095f6;
+					color: white;
+					border: 1px solid transparent;
+				`;
+			case "smallWhite":
+				return css`
+					width: auto;
+					background-color: white;
+					color: #0095f6;
+					border: 1px solid transparent;
+				`;
+			case "longBlue":
+				return css`
+					width: 268px;
+					height: 30px;
+					background-color: #0095f6;
+					color: white;
+				`;
+			case "longWhite":
+				return css`
+					width: 268px;
+					height: 30px;
+					background-color: white;
+					color: #002952;
+				`;
+			default:
+				break;
+		}
+	}};
+`;
