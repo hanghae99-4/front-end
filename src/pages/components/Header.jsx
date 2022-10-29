07de7b54de8fe04555d4881f9430ef0b7e2c6037
NyeongDev <90minyeong90@gmail.com>
2022-10-29 13:36:32 +0900

    Modify Header style

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,19 +1,22 @@
 import Div from "../../common/Div";
 import Input from "../../common/Input";
 import Layout from "../../common/Layout";
+import Margin from "../../common/Margin";
 import Nav from "../../common/Nav";
 
 const Header = () => {
 	return (
 		<Div variant="header">
 			<Layout variant="header">
-				<img
-					width="103"
-					height="29"
-					objectFit="cover"
-					alt=""
-					src="/images/Instagram-Wordmark.png"
-				/>
+				<Margin margin="5px 0 0 0">
+					<img
+						width="103"
+						height="29"
+						objectFit="cover"
+						alt=""
+						src="/images/Instagram-Wordmark.png"
+					/>
+				</Margin>
 				<Input variant="search" />
 				<Nav />
 			</Layout>
