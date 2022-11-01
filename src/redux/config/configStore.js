5b3dae8a8db44d0f652d48c1c5f0d3a001f46ba8
NyeongDev <90minyeong90@gmail.com>
2022-11-01 20:52:36 +0900

    Modify upload function

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalSlice from "../modules/modalSlice";
 import users from "../modules/usersSlice";
+import feedSlice from "../modules/feedSlice";
 
 const store = configureStore({
 	reducer: {
 		users,
 		modalSlice,
+		feedSlice,
 	},
 });
 
