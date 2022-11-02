8410ea6b7be2ed4431e4aff954e8f320c080dbb4
NyeongDev <90minyeong90@gmail.com>
2022-11-02 09:27:36 +0900

    Modify connection between profilepage and detail modal

--- src/pages/profile/components/ProfileFeedsArea.jsx
+++ src/pages/profile/components/ProfileFeedsArea.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
 import Image from "../../../common/Image";
 import Layout from "../../../common/Layout";
+import { getFeedItem } from "../../../redux/modules/feedSlice";
 import { updateDetailModalOpen } from "../../../redux/modules/modalSlice";
 
 const ProfileFeedsArea = ({ feedItem }) => {
@@ -16,7 +17,8 @@ const ProfileFeedsArea = ({ feedItem }) => {
 	} = feedItem;
 	const dispatch = useDispatch();
 	const OpenModal = () => {
-		dispatch(updateDetailModalOpen());
+		dispatch(getFeedItem(feedItem));
+		dispatch(updateDetailModalOpen(feedId));
 	};
 	return (
 		<Layout variant="profileFeedsLayout">
