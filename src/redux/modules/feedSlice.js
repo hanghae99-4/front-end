725a4c2c2bcb9e238785e210b3aebbad74ccddf0
NyeongDev <90minyeong90@gmail.com>
2022-11-02 17:32:20 +0900

    Modify Profile page layout

--- src/redux/modules/feedSlice.js
+++ src/redux/modules/feedSlice.js
@@ -141,9 +141,13 @@ export const feedSlice = createSlice({
 		},
 	},
 	extraReducers: {
-		// 게시물 업로드
+		//! 게시물 업로드
+		[__addFeed.pending]: (state, action) => {
+			state.isAddFeedLoading = true;
+		},
 		[__addFeed.fulfilled]: (state, action) => {
 			// console.log("@ __addFeed fullfilled", action.payload);
+			alert("게시물이 공유되었습니다.");
 			state.feedItem = action.payload;
 			// console.log("@ __addFeed state change", state.feedItem);
 		},
@@ -151,7 +155,7 @@ export const feedSlice = createSlice({
 			// console.log("@ __addFeed rejected", action.payload);
 		},
 
-		// 프로필 페이지 조회
+		//! 프로필 페이지 조회
 		[__getProFileFeedList.fulfilled]: (state, action) => {
 			// console.log("@ __getProFileFeedList fullfilled", action.payload);
 			state.profileFeedList = action.payload;
@@ -161,7 +165,7 @@ export const feedSlice = createSlice({
 			// console.log("@ __getProFileFeedList rejected", action.payload);
 		},
 
-		// 메인 페이지 조회
+		//! 메인 페이지 조회
 		[__getMainFeedList.fulfilled]: (state, action) => {
 			// console.log("@ __getMainFeedList fullfilled", action.payload);
 			state.mainFeedList = action.payload.data;
@@ -171,7 +175,7 @@ export const feedSlice = createSlice({
 			// console.log("@ __getMainFeedList rejected", action.payload);
 		},
 
-		// 게시물 삭제
+		//! 게시물 삭제
 		[__delFeedItem.fulfilled]: (state, action) => {
 			// console.log("@ __delFeedItem fullfilled", action.payload);
 			if (action.payload.success === false) {
@@ -184,6 +188,14 @@ export const feedSlice = createSlice({
 		},
 		[__delFeedItem.rejected]: (state, action) => {
 			// console.log("@ __delFeedItem rejected", action.payload);
+		},
+
+		//! 게시물 수정
+		[__updateFeedItem.fulfilled]: (state, action) => {
+			console.log("@ __updateFeedItem fullfilled", action.payload);
+		},
+		[__updateFeedItem.rejected]: (state, action) => {
+			console.log("@ __updateFeedItem rejected", action.payload);
 		},
 	},
 });
